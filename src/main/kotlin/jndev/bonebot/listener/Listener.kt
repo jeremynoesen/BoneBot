b3ea10a72fa43a5e4929fa8f3210258a5dd1bdec
Jeremy Noesen <jeremynoesen@gmail.com>
2020-12-04 18:44:42 -0600

    help command

--- src/main/kotlin/jndev/bonebot/listener/Listener.kt
+++ src/main/kotlin/jndev/bonebot/listener/Listener.kt
@@ -5,8 +5,10 @@ import jndev.bonebot.modules.Command
 import jndev.bonebot.modules.Meme
 import jndev.bonebot.modules.Reactor
 import jndev.bonebot.modules.Responder
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.awt.Color
 
 /**
  * all listeners for the bot
@@ -29,8 +31,27 @@ class Listener : ListenerAdapter() {
                 e.message.contentRaw.startsWith(Config.commandPrefix + "restart") -> {
                     e.channel.sendMessage("Restarting...").queue()
                     e.channel.sendTyping().queue()
-                    Thread.sleep(100L)
+                    Thread.sleep(500L)
                     System.exit(0)
+                }
+                e.message.contentRaw.startsWith(Config.commandPrefix + "help") -> {
+                    var commandList = ""
+                    var i = 0;
+                    for(command in Command.commands) {
+                        commandList += Config.commandPrefix + command.split(" // ")[0]
+                        if(i != Command.commands.size - 1) commandList += ", "
+                        i++
+                    }
+                    val embedBuilder = EmbedBuilder()
+                    embedBuilder.setTitle("BoneBot Help")
+                    embedBuilder.setDescription("The following are built-in BoneBot commands:\n\n" +
+                                Config.commandPrefix + "meme <optional text> <optional image or user ping> - Generate" +
+                                " a meme with input or random text and input or random image. Ping a user to use their avatar!\n\n" +
+                                Config.commandPrefix + "restart - restarts the bot if set up properly, otherwise shuts down\n\n\n" +
+                                "The following is a list of custom commands added to the bot by the manager of this bot:\n\n" +
+                                commandList)
+                    embedBuilder.setColor(Color.BLUE)
+                    e.channel.sendMessage(embedBuilder.build()).queue()
                 }
                 else -> {
                     Responder.respond(e.message)
