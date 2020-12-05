90cbe4ec61609159e32f88ba79fde9170631ed2d
Jeremy Noesen <jeremynoesen@gmail.com>
2020-12-04 18:54:54 -0600

    restart command improvements

--- src/main/kotlin/jndev/bonebot/listener/Listener.kt
+++ src/main/kotlin/jndev/bonebot/listener/Listener.kt
@@ -29,27 +29,32 @@ class Listener : ListenerAdapter() {
                     Runtime.getRuntime().gc()
                 }
                 e.message.contentRaw.startsWith(Config.commandPrefix + "restart") -> {
-                    e.channel.sendMessage("Restarting...").queue()
-                    e.channel.sendTyping().queue()
-                    Thread.sleep(500L)
-                    System.exit(0)
+                    if (e.author == e.guild.owner?.user) {
+                        e.channel.sendMessage("Restarting...").queue()
+                        e.channel.sendTyping().queue()
+                        e.jda.shutdown()
+                    } else {
+                        e.message.delete().queue()
+                    }
                 }
                 e.message.contentRaw.startsWith(Config.commandPrefix + "help") -> {
                     var commandList = ""
                     var i = 0;
-                    for(command in Command.commands) {
+                    for (command in Command.commands) {
                         commandList += Config.commandPrefix + command.split(" // ")[0]
-                        if(i != Command.commands.size - 1) commandList += ", "
+                        if (i != Command.commands.size - 1) commandList += ", "
                         i++
                     }
                     val embedBuilder = EmbedBuilder()
                     embedBuilder.setTitle("BoneBot Help")
-                    embedBuilder.setDescription("The following are built-in BoneBot commands:\n\n" +
+                    embedBuilder.setDescription(
+                        "The following are built-in BoneBot commands:\n\n" +
                                 Config.commandPrefix + "meme <optional text> <optional image or user ping> - Generate" +
                                 " a meme with input or random text and input or random image. Ping a user to use their avatar!\n\n" +
                                 Config.commandPrefix + "restart - restarts the bot if set up properly, otherwise shuts down\n\n\n" +
                                 "The following is a list of custom commands added to the bot by the manager of this bot:\n\n" +
-                                commandList)
+                                commandList
+                    )
                     embedBuilder.setColor(Color.BLUE)
                     e.channel.sendMessage(embedBuilder.build()).queue()
                 }
