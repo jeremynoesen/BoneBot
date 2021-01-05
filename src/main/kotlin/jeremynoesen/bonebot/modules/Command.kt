7010af31461c7f4599228b1c95ab8984bb16e04d
Jeremy Noesen <jeremynoesen@gmail.com>
2021-01-05 17:06:30 -0600

    refactoring

--- src/main/kotlin/jeremynoesen/bonebot/modules/Command.kt
+++ src/main/kotlin/jeremynoesen/bonebot/modules/Command.kt
@@ -1,7 +1,9 @@
 package jeremynoesen.bonebot.modules
 
 import jeremynoesen.bonebot.Logger
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.Message
+import java.awt.Color
 
 /**
  * command handler with simple message responses
@@ -39,20 +41,59 @@ object Command {
         try {
             val msg = message.contentRaw.toLowerCase()
             if (msg.startsWith(commandPrefix)) {
-                for (command in commands.keys) {
-                    if (msg.startsWith("$commandPrefix$command")) {
-                        if ((System.currentTimeMillis() - prevTime) >= cooldown * 1000) {
-                            prevTime = System.currentTimeMillis()
-                            message.channel.sendMessage(
-                                commands[command]!!.replace(
-                                    "\$USER$",
-                                    message.author.asMention
-                                )
-                            ).queue()
+                when {
+                    msg.startsWith(commandPrefix + "meme") -> {
+                        Meme(message).generate()
+                    }
+                    msg.startsWith(commandPrefix + "restart") -> {
+                        if (message.member!!.isOwner) {
+                            message.channel.sendMessage("Restarting...").queue()
+                            message.channel.sendTyping().queue()
+                            message.jda.shutdown()
+                            Thread.sleep(1000)
+                            System.exit(0)
                         } else {
                             message.delete().queue()
                         }
-                        break
+                    }
+                    msg.startsWith(commandPrefix + "help") -> {
+                        var commandList = ""
+                        for ((i, command) in commands.keys.withIndex()) {
+                            commandList += "`$commandPrefix$command`"
+                            if (i != commands.size - 1) commandList += ", "
+                        }
+                        if (commandList.isBlank()) commandList = "*No commands defined*"
+                        val embedBuilder = EmbedBuilder()
+                        val name = message.jda.selfUser.name
+                        embedBuilder.setAuthor("$name Help", null, message.jda.selfUser.avatarUrl)
+                        embedBuilder.setColor(Color(0, 151, 255))
+                        embedBuilder.setDescription(
+                            "**Default Commands**\n" +
+                                    "• `" + commandPrefix + "meme <text> <image or user>`: Generate" +
+                                    " a meme using text and image or user avatar. If configured, you" +
+                                    " can skip either to randomly pick them.\n" +
+                                    "• `" + commandPrefix + "restart`: Restart the bot if configured, otherwise shut down.\n\n" +
+                                    "**Custom Commands**\n" + commandList + "\n\n[GitHub](https://github.com/jeremynoesen/BoneBot)"
+                        )
+                        message.channel.sendMessage(embedBuilder.build()).queue()
+                    }
+                    else -> {
+                        for (command in commands.keys) {
+                            if (msg.startsWith("$commandPrefix$command")) {
+                                if ((System.currentTimeMillis() - prevTime) >= cooldown * 1000) {
+                                    prevTime = System.currentTimeMillis()
+                                    message.channel.sendMessage(
+                                        commands[command]!!.replace(
+                                            "\$USER$",
+                                            message.author.asMention
+                                        )
+                                    ).queue()
+                                } else {
+                                    message.delete().queue()
+                                }
+                                break
+                            }
+                        }
                     }
                 }
             }
