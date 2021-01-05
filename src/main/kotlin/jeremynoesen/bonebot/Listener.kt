acb2bfd3a4cc46ebb974cb036ae4829e951e12de
Jeremy Noesen <jeremynoesen@gmail.com>
2021-01-05 17:06:30 -0600

    refactoring

--- src/main/kotlin/jeremynoesen/bonebot/Listener.kt
+++ src/main/kotlin/jeremynoesen/bonebot/Listener.kt
@@ -1,13 +1,10 @@
 package jeremynoesen.bonebot
 
 import jeremynoesen.bonebot.modules.Command
-import jeremynoesen.bonebot.modules.Meme
 import jeremynoesen.bonebot.modules.Reactor
 import jeremynoesen.bonebot.modules.Responder
-import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import java.awt.Color
 
 /**
  * all listeners for the bot
@@ -17,55 +14,16 @@ import java.awt.Color
 class Listener : ListenerAdapter() {
 
     /**
-     * respond to users when they say certain key words
+     * respond and/or react to users when they say certain key words
      *
      * @param e message received event
      */
     override fun onMessageReceived(e: MessageReceivedEvent) {
         try {
             if (!e.author.isBot) {
-                when {
-                    e.message.contentRaw.startsWith(Command.commandPrefix + "meme") -> {
-                        Meme(e.message).generate()
-                    }
-                    e.message.contentRaw.startsWith(Command.commandPrefix + "restart") -> {
-                        if (e.member!!.isOwner) {
-                            e.channel.sendMessage("Restarting...").queue()
-                            e.channel.sendTyping().queue()
-                            e.jda.shutdown()
-                            Thread.sleep(1000)
-                            System.exit(0)
-                        } else {
-                            e.message.delete().queue()
-                        }
-                    }
-                    e.message.contentRaw.startsWith(Command.commandPrefix + "help") -> {
-                        var commandList = ""
-                        for ((i, command) in Command.commands.keys.withIndex()) {
-                            commandList += "`${Command.commandPrefix}$command`"
-                            if (i != Command.commands.size - 1) commandList += ", "
-                        }
-                        if (commandList.isBlank()) commandList = "*No commands defined*"
-                        val embedBuilder = EmbedBuilder()
-                        val name = e.jda.selfUser.name
-                        embedBuilder.setAuthor("$name Help", null, e.jda.selfUser.avatarUrl)
-                        embedBuilder.setColor(Color(0, 151, 255))
-                        embedBuilder.setDescription(
-                            "**Default Commands**\n" +
-                                    "• `" + Command.commandPrefix + "meme <text> <image or user>`: Generate" +
-                                    " a meme using text and image or user avatar. If configured, you" +
-                                    " can skip either to randomly pick them.\n" +
-                                    "• `" + Command.commandPrefix + "restart`: Restart the bot if configured, otherwise shut down.\n\n" +
-                                    "**Custom Commands**\n" + commandList + "\n\n[GitHub](https://github.com/jeremynoesen/BoneBot)"
-                        )
-                        e.channel.sendMessage(embedBuilder.build()).queue()
-                    }
-                    else -> {
-                        Command.perform(e.message)
-                        Responder.respond(e.message)
-                        Reactor.react(e.message)
-                    }
-                }
+                Command.perform(e.message)
+                Responder.respond(e.message)
+                Reactor.react(e.message)
             }
         } catch (e: Exception) {
             Logger.log(e)
