cd606822c575b770b988754269e34b349bee2ba7
Jeremy Noesen <jeremynoesen@gmail.com>
2021-01-05 16:15:28 -0600

    fix status not changing on bot start

--- src/main/kotlin/jeremynoesen/bonebot/modules/Status.kt
+++ src/main/kotlin/jeremynoesen/bonebot/modules/Status.kt
@@ -30,18 +30,18 @@ object Status {
      */
     fun setStatus(jda: JDA) {
         Thread {
+            try {
+                if(statuses.isNotEmpty()) {
+                    val random = Random()
+                    jda.presence.activity = Activity.playing(statuses[random.nextInt(statuses.size)])
+                }
+            } catch (e: Exception) {
+                Logger.log(e)
+            }
             while (true) {
                 try {
                     Thread.sleep(cooldown * 1000L)
                 } catch (e: InterruptedException) {
-                    Logger.log(e)
-                }
-                try {
-                    if(statuses.isNotEmpty()) {
-                        val random = Random()
-                        jda.presence.activity = Activity.playing(statuses[random.nextInt(statuses.size)])
-                    }
-                } catch (e: Exception) {
                     Logger.log(e)
                 }
             }
